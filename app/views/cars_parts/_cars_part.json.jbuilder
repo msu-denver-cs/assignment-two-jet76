d7caec1848a62cea2657f8262c4acc1f0a8d9151
Joe Turner <joe.turner@live.com>
2019-09-21 18:15:24 -0600

    Initial commit

---
+++ app/views/cars_parts/_cars_part.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! cars_part, :id, :car_id, :part_id, :created_at, :updated_at
+json.url cars_part_url(cars_part, format: :json)
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :cars_parts
+  resources :cars
+  resources :makes
+  resources :parts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
